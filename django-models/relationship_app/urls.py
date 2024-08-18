20a763a3f62f9b811559e407b559b25e34147324
aeikun <aeibouyhy@gmail.com>
2024-08-18 21:55:48 +0100

    Fix template reference

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, RegisterView, admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
 
 
 urlpatterns = [
@@ -12,4 +13,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
