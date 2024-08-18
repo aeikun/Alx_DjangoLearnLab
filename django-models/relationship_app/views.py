3dd41c48053b7d356eec170b7164ca2c0a4215c5
aeikun <aeibouyhy@gmail.com>
2024-08-18 15:17:57 +0100

    Implement user authentication views and templates

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import View
+from django.contrib.auth import login
 
 # Function-based view to list all books
 def list_books(request):
