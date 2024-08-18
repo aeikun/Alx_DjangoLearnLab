c2e8f126289621797d71e898796273c212606bc7
aeikun <aeibouyhy@gmail.com>
2024-08-18 18:53:52 +0100

    Implement user authentication with login, logout, and registration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 
 def list_books(request):
