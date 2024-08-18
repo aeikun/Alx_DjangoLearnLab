9fcf751204ec45f5caed4f19fc2a5002a10deb12
aeikun <aeibouyhy@gmail.com>
2024-08-18 14:56:22 +0100

    Add function-based and class-based views to relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
