994af7eb6dbbbb510843dab844f70f67d6c97b12
aeikun <aeibouyhy@gmail.com>
2024-08-18 14:50:48 +0100

    Add function-based and class-based views to relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,10 +4,10 @@ from .models import Book
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'templates/list_books.html', {'books': books})
+    return render(request, 'list_books.html', {'books': books})
 
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'templates/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
