702008c5f86f5ccb7f9b36848dae6655145fc311
aeikun <aeibouyhy@gmail.com>
2024-08-19 19:48:14 +0100

    Fix template reference

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -55,28 +55,25 @@ def logout_view(request):
 
 # Role-based views
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
-@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request,'relationship_app/admin_view.html')
 
-@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request,'relationship_app/librarian_view.html')
 
-@login_required
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request,'relationship_app/member_view.html')
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
