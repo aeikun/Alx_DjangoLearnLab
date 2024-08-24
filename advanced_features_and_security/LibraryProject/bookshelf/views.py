c93b91e73e9b963fdd51f0abb6fb4d2b6aab7a75
aeikun <aeibouyhy@gmail.com>
2024-08-24 22:04:44 +0100

    Advanced Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,17 @@ from django.http import HttpResponse
 from django.core.exceptions import PermissionDenied
 from .models import Book
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success_url')  # Replace with your success URL
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_list(request):
@@ -29,3 +40,9 @@ def raise_exception(request):
 def books(request):
     """Another view for handling books (can be customized as needed)."""
     return HttpResponse("This is the books view.")
+
+# Unsafe (potential SQL injection)
+books = Book.objects.raw('SELECT * FROM bookshelf_book WHERE title = "%s"' % title)
+
+# Safe
+books = Book.objects.filter(title=title)
