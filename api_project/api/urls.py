e3d98e619230ca0ff37f1858733456f2a6c3de3a
aeikun <aeibouyhy@gmail.com>
2024-08-26 20:00:46 +0100

    Building APIs with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,5 +5,5 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api.urls/', include('api.urls')),
 ]
