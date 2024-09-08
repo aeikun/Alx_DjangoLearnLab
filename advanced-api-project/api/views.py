06c268fd0d85dedf779920d8d3ac2a4a7e37dfd2
aeikun <aeibouyhy@gmail.com>
2024-09-08 20:20:19 +0100

    creating and configuring custom views using Django REST Framework’s powerful generic views and mixins.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,24 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import filters
+
 
 class BookListView(generics.ListCreateAPIView):
+    """
+    Retrieve all books or create a new book.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AllowAny]  # Change this based on your requirement
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'publication_year']
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a specific book by ID.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
