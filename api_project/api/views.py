83ff551328aaf3420de85cf3a059d82fb86e2a1b
aeikun <aeibouyhy@gmail.com>
2024-09-10 19:54:22 +0100

    creating and configuring custom views using Django REST Framework’s powerful generic views and mixins.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter, NumberFilter
 from .models import Book
 from .serializers import BookSerializer
