cba96ddeb6deb4ae7f46f7c5a099da9edb3e57d2
aeikun <aeibouyhy@gmail.com>
2024-09-10 19:45:50 +0100

    creating and configuring custom views using Django REST Framework’s powerful generic views and mixins.

--- 0x1.Introduction_ to_ Django/LibraryProject/delete.md
+++
@@ -1,7 +0,0 @@
-# Delete Operation
-
-Command:
-```python
-book = Book.objects.get(title='Nineteen Eighty-Four')
-book.delete()
-print(Book.objects.all())
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -15,3 +15,4 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
