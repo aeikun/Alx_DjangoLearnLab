e30785ee30d23f3e07e38b1ae4ec090442a66e0c
aeikun <aeibouyhy@gmail.com>
2024-09-11 19:24:35 +0100

    creating and configuring custom views using Django REST Framework’s powerful generic views and mixins.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter, NumberFilter
 from .models import Book
 from .serializers import BookSerializer
@@ -18,7 +19,7 @@ class BookFilter(FilterSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = (DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
     filterset_class = BookFilter
     search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
