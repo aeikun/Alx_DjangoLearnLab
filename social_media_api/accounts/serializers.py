7e360a9221da5a45535f6b5c13f403132abaa792
aeikun <aeibouyhy@gmail.com>
2024-09-19 22:41:41 +0100

    establishing the foundational elements of your Social Media API.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Ensure password is write-only
     class Meta:
         model = CustomUser
         fields = ('username', 'password', 'bio', 'profile_picture')
@@ -13,4 +17,6 @@ class UserSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        token = Token.objects.create(user=user)
+        
         return user
